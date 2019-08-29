a09858ce42275f1b465eb7e9473557aae1d61f63
Rosama AIsa <604234018@parichat.skru.ac.th>
2019-08-29 17:07:49 +0700

    update

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import {HttpClientModule} from '@angular/common/http';
+import { JsonMenuPage } from './../pages/json-menu/json-menu';
+import { Flashlight } from '@ionic-native/flashlight';
+import { FlashlightPage } from './../pages/flashlight/flashlight';
+import { ScannerPage } from './../pages/scanner/scanner';
+import { SMS } from '@ionic-native/sms/ngx';
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { LoginPage } from '../pages/login/login';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,LoginPage,ScannerPage,FlashlightPage,JsonMenuPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,LoginPage,ScannerPage,FlashlightPage,JsonMenuPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen, SMS,BarcodeScanner,Flashlight,JsonMenuPage,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
